a6043360f62892c6e65fcab0f85dc41681e9f60a
Danu <danu3006@gmail.com>
2019-09-02 15:15:32 +0100

    update: Cleaner block detail page

--- packages/web/src/containers/Block.tsx
+++ packages/web/src/containers/Block.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { Babble, IBabbleBlock } from 'evm-lite-consensus';
 import { JsonToTable } from 'react-json-to-table';
 import { RouteComponentProps } from 'react-router-dom';
-import { Container, Grid } from 'semantic-ui-react';
+import { Container, Grid, Table } from 'semantic-ui-react';
 
 import Node from 'evm-lite-core';
 
@@ -46,12 +46,54 @@ const Index: React.FC<RouteComponentProps<IProps>> = props => {
 		fetchBlock();
 	}, []);
 
+	const renderBody = () => {
+		if (Object.keys(block).length) {
+			return Object.keys(block.Body).map(k => {
+				// @ts-ignore
+				const val = block.Body[k];
+
+				return (
+					<Table.Row key={block.Body.Index}>
+						<Table.Cell>{k}</Table.Cell>
+						<Table.Cell>{val}</Table.Cell>
+					</Table.Row>
+				);
+			});
+		}
+	};
+
+	const renderSignatures = () => {
+		if (Object.keys(block).length) {
+			return Object.keys(block.Signatures).map(k => {
+				// @ts-ignore
+				const val = block.Body[k];
+
+				return (
+					<Table.Row key={block.Body.Index}>
+						<Table.Cell>{k}</Table.Cell>
+						<Table.Cell>{val}</Table.Cell>
+					</Table.Row>
+				);
+			});
+		}
+	};
+
 	return (
 		<Container fluid={true}>
 			<Grid stackable={true} columns={'equal'}>
 				<Grid.Column>
-					<Box padding={true} heading={`Block - ${index}`}>
-						<JsonToTable json={block} />
+					<h2>Block {index}</h2>
+					<Box padding={false} heading={`Body`}>
+						<Table>
+							<Table.Header>
+								<Table.HeaderCell>Key</Table.HeaderCell>
+								<Table.HeaderCell>Value</Table.HeaderCell>
+							</Table.Header>
+							<Table.Body>{renderBody()}</Table.Body>
+						</Table>
+					</Box>
+					<Box padding={true} heading={`Signatures`}>
+						<JsonToTable json={block.Signatures} />
 					</Box>
 				</Grid.Column>
 			</Grid>
