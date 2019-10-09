4fbaadcdb48e962f77cb9750974390404f6b1550
Danu <danu3006@gmail.com>
2019-10-09 15:13:57 +0100

    update: Production ready mode starting

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(dashboard.monet.network)
 rsa_key_size=4096
 data_path="./docker/certbot"
 email="danushan@babble.io" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -42,12 +42,12 @@ echo "### Starting nginx ..."
 docker-compose up --force-recreate -d nginx
 echo
 
-# echo "### Deleting dummy certificate for $domains ..."
-# docker-compose run --rm --entrypoint "\
-#   rm -Rf /etc/letsencrypt/live/$domains && \
-#   rm -Rf /etc/letsencrypt/archive/$domains && \
-#   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
-# echo
+echo "### Deleting dummy certificate for $domains ..."
+docker-compose run --rm --entrypoint "\
+  rm -Rf /etc/letsencrypt/live/$domains && \
+  rm -Rf /etc/letsencrypt/archive/$domains && \
+  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+echo
 
 
 echo "### Requesting Let's Encrypt certificate for $domains ..."
