d78b99c6e65473023b1bcf711f90e297503027a2
Danu <danu3006@gmail.com>
2019-10-09 14:39:39 +0100

    update: Semi-finished docker-letsencrypt set up

---
+++ build/precache-manifest.a55bb3f6039c68d5fa159eaba017b9fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f614126baa62f8ae2b7f9ca51af42f1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "125c5aa94925a70bb26e",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "0df587bbed55ce05642b",
+    "url": "/static/css/main.a80cc9d5.chunk.css"
+  },
+  {
+    "revision": "125c5aa94925a70bb26e",
+    "url": "/static/js/2.92bc0a98.chunk.js"
+  },
+  {
+    "revision": "0df587bbed55ce05642b",
+    "url": "/static/js/main.941d0ca1.chunk.js"
+  },
+  {
+    "revision": "7c5acfd15a7cbd23585f",
+    "url": "/static/js/runtime-main.e50ad8c6.js"
+  },
+  {
+    "revision": "d44a0bfac91920fa819742623c503ed6",
+    "url": "/static/media/bg.d44a0bfa.png"
+  },
+  {
+    "revision": "c256ca5b58eef15c5bad76769dc39ae1",
+    "url": "/static/media/monet.c256ca5b.svg"
+  }
+]);
