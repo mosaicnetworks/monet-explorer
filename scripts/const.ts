20cfe22427f61002132a3478d0a88067c67ec61a
Danu <danu3006@gmail.com>
2019-10-11 14:26:46 +0100

    update: Better develpoment environment and django pre build

--- scripts/const.ts
+++
@@ -1,2 +0,0 @@
-export const HOST: string = 'localhost';
-export const PORT: number = 8080;
---
+++ scripts/pre-django.sh
@@ -0,0 +1,5 @@
+python3 pip3 -m install -r ./server/requirements.txt
+
+python3 ./server/src/manage.py makemigrations
+python3 ./server/src/manage.py makemigrations core
+python3 ./server/src/manage.py migrate
