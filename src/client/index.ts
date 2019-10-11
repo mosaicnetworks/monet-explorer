cf270d2d82ad7de2cce31705c412fc0157656a73
Danu <danu3006@gmail.com>
2019-10-11 15:31:17 +0100

    update: Added Nodejs client for API

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,32 @@
-class ExplorerAPIClient {
+import { AbstractClient } from 'evm-lite-client';
+
+type Network = {
+	id: number;
+	name: string;
+	host: string;
+	port: number;
+};
+
+class ExplorerAPIClient extends AbstractClient {
 	constructor() {
-		// pass
+		super('localhost', 8000);
+	}
+
+	public async getNetworks(): Promise<Network[]> {
+		return JSON.parse(await this.get('/api/networks/'));
+	}
+
+	public async getValidators(
+		network: 'camille' | string
+	): Promise<Network[]> {
+		return JSON.parse(
+			await this.get(`/api/validators/?network=${network}`)
+		);
+	}
+
+	public async getInfos(network: 'camille' | string): Promise<Network[]> {
+		return JSON.parse(await this.get(`/api/infos/?network=${network}`));
 	}
 }
+
+export default ExplorerAPIClient;
