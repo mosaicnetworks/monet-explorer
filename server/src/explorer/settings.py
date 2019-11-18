339a6db7c3166a149ec4d48fd45be3d672fa1e07
Danu <danu3006@gmail.com>
2019-11-18 15:25:15 +0000

    update: Remove session authentication DRF

--- server/src/explorer/settings.py
+++ server/src/explorer/settings.py
@@ -154,8 +154,6 @@ CRONJOBS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_RENDERER_CLASSES': (
