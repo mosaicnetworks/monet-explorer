c05b691f5e9ab92cb922171343b3cdbe7e469ccb
Danu <danu3006@gmail.com>
2019-11-19 13:28:02 +0000

    update: Debug to true django

--- server/src/explorer/settings.py
+++ server/src/explorer/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$k0-#o#^!$jfn$(q$418pclze89kg3i28v9!)$@sjuy=(pdozr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
